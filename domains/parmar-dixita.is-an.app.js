d552e092d8d1d6a8dffffd3ee264537292a1d25f
parmar-dixita <dixita.parmar@truestaz.com>
2023-04-21 15:13:08 +0530

    Add .is-an.app.js File

---
+++ domains/parmar-dixita.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'parmar-dixita',
+  owner: {
+    repo: 'https://github.com/parmar-dixita/parmar-dixita.github.io',
+    email: 'dixita.parmar@truestaz.com',
+  },
+  record: {
+    CNAME: 'parmar-dixita.github.io',
+  },
+  proxy: false,
+})
